935a8127faacc39da24c4634971c53bdec5a45e1
Jutmzg <jul.bousseau@gmail.com>
2019-07-02 11:19:32 +0200

    quete 17.1 User

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Service\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -10,6 +11,13 @@ use Faker;
 
 class ArticleFixtures extends Fixture implements DependentFixtureInterface
 {
+    private $slugify;
+
+    public function __construct(Slugify $slugify)
+    {
+        $this->slugify =$slugify;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
@@ -19,6 +27,7 @@ class ArticleFixtures extends Fixture implements DependentFixtureInterface
             $article = new Article();
             $article->setTitle(mb_strtolower($faker->sentence()));
             $article->setContent($faker->text(200));
+            $article->setSlug($this->slugify->generate($article->getTitle()));
             $manager->persist($article);
             $article->setCategory($this->getReference('categorie_' . rand(0,4)));
         }
